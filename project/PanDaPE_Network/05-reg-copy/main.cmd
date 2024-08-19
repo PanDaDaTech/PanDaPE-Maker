b4c3fe45bc1382554d760d184a40bed4e22765e3
PanDaTech <66164680+PanDaDaTech@users.noreply.github.com>
2024-08-19 21:05:41 +0800

    Add files via upload

--- project/PanDaPE_Network/05-reg-copy/main.cmd
+++ project/PanDaPE_Network/05-reg-copy/main.cmd
@@ -1,29 +1,24 @@
 @echo off
 
-echo 提取 install 注册表文件
-wimlib-imagex.exe extract "%install%" %installinfo% "\Windows\System32\config\DEFAULT" --dest-dir="%C%" --nullglob --no-acls
-wimlib-imagex.exe extract "%install%" %installinfo% "\Windows\System32\config\DRIVERS" --dest-dir="%C%" --nullglob --no-acls
-wimlib-imagex.exe extract "%install%" %installinfo% "\Windows\System32\config\SOFTWARE" --dest-dir="%C%" --nullglob --no-acls
-wimlib-imagex.exe extract "%install%" %installinfo% "\Windows\System32\config\SYSTEM" --dest-dir="%C%" --nullglob --no-acls
+echo 删除 Winners 注册表
+reg delete HKEY_LOCAL_MACHINE\Tmp_SOFTWARE\Microsoft\Windows\CurrentVersion\SideBySide\Winners /f
 
-echo 挂载注册表
-reg load HKLM\Src_DEFAULT %C%\DEFAULT
-reg load HKLM\Src_DRIVERS %C%\DRIVERS
-reg load HKLM\Src_SOFTWARE %C%\SOFTWARE
-reg load HKLM\Src_SYSTEM %C%\SYSTEM
+echo 复制必要的 Winners 注册表
+for /f "delims=" %%i in (%cd%\Winners.txt) do (call RegCopy HKLM\Software\Microsoft\Windows\CurrentVersion\SideBySide\Winners,%%i)
 
-reg load HKLM\Tmp_DEFAULT %X%\Windows\System32\config\DEFAULT
-reg load HKLM\Tmp_DRIVERS %X%\Windows\System32\config\DRIVERS
-reg load HKLM\Tmp_SOFTWARE %X%\Windows\System32\config\SOFTWARE
-reg load HKLM\Tmp_SYSTEM %X%\Windows\System32\config\SYSTEM
+echo 复制必要的注册表
+for /r "%cd%\DEFAULT" %%i in (*.txt) do (echo %%i&for /f "delims=" %%i in (%cd%\DEFAULT\%%~nxi) do (call RegCopy "%%i"))
+for /r "%cd%\DRIVERS" %%i in (*.txt) do (echo %%i&for /f "delims=" %%i in (%cd%\DRIVERS\%%~nxi) do (call RegCopy "%%i"))
+for /r "%cd%\SOFTWARE" %%i in (*.txt) do (echo %%i&for /f "delims=" %%i in (%cd%\SOFTWARE\%%~nxi) do (call RegCopy "%%i"))
+for /r "%cd%\SYSTEM" %%i in (*.txt) do (echo %%i&for /f "delims=" %%i in (%cd%\SYSTEM\%%~nxi) do (call RegCopy "%%i"))
 
-echo 获取注册表权限
-setacl.exe -on "HKLM\Tmp_DRIVERS" -ot reg -actn setowner -ownr "n:Everyone" -rec yes -silent
-setacl.exe -on "HKLM\Tmp_DRIVERS" -ot reg -actn ace -ace "n:Everyone;p:full;m:grant;i:so,sc" -op DACL:p_c -rec yes -silent
-echo drivers 获取完成
-setacl.exe -on "HKLM\Tmp_SOFTWARE" -ot reg -actn setowner -ownr "n:Everyone" -rec yes -silent
-setacl.exe -on "HKLM\Tmp_SOFTWARE" -ot reg -actn ace -ace "n:Everyone;p:full;m:grant;i:so,sc" -op DACL:p_c -rec yes -silent
-echo software 获取完成
-setacl.exe -on "HKLM\Tmp_SYSTEM" -ot reg -actn setowner -ownr "n:Everyone" -rec yes -silent
-setacl.exe -on "HKLM\Tmp_SYSTEM" -ot reg -actn ace -ace "n:Everyone;p:full;m:grant;i:so,sc" -op DACL:p_c -rec yes -silent
-echo system 获取完成
+echo 补充 RNDIS 注册表
+call RegCopy HKLM\Drivers\DriverDatabase\DeviceIds\{4d36e972-e325-11ce-bfc1-08002be10318}
+call RegCopy HKLM\Drivers\DriverDatabase\DeviceIds\ms_rndisusb
+call RegCopy HKLM\Drivers\DriverDatabase\DeviceIds\ms_rndisusb6
+call RegCopy HKLM\Drivers\DriverDatabase\DeviceIds\USB\Class_EF&SubClass_04&Prot_01
+call RegCopy HKLM\Drivers\DriverDatabase\DeviceIds\USB\MS_COMP_RNDIS&MS_SUBCOMP_5162001
+
+call RegCopy /-s HKLM\Software\Microsoft\Windows NT\CurrentVersion
+
+reg delete HKLM\Tmp_SOFTWARE\Microsoft\Windows\CurrentVersion\WINEVT /f
